144dc10888f948618410142f216fa2e85b8aa5bb
Jacob Dent <jdent@tacyarg.com>
2019-11-18 23:03:58 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div class="overlay"></div>
+    <router-view/>
+  </div>
+</template>
+
+<style scoped>
+ #app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  overflow-y: auto;
+ }
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0; 
+    left: 0;
+    background: url('./assets/background.png') no-repeat center center fixed; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    opacity: 0.1;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="home">
+    <img class="logo" src="@/assets/logo.png">
+    <div class="description">
+      Our Mexicans are currently cooking up a fiesta in the background,
+      to bring back the good ol’ famous Fiesta, that we all knew and loved
+      but with a twist…We’ve redecorated. We’re excited to show you all! 
+    </div>
+    <a href="https://twitter.com/thecsgofiesta" target="_blank"><div class="button">Keep up to date, via our Twitter</div></a>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+
+<style scoped>
+  .home {
+    position: relative;
+    z-index: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+  }
+
+  .logo {
+    max-width: 327px;
+  }
+
+  .description {
+    max-width: 422px;
+    color: #E4E6FA;
+    margin: 30px 50px;
+    text-align: center;
+    opacity: 0.4;
+    line-height: 1.5em;
+
+  }
+
+  .button {
+    width: 272px;
+    height: 70px;
+    background: #272655;
+    border-radius: 5px;
+    color: #E4E6FA;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    transition: 0.3s all ease-in-out;
+  }
+
+  .button:hover {
+    font-size: 1.1em;
+    font-weight: bold;
+    color: #272655;
+    background: #E4E6FA;
+  }
+</style>
